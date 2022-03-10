03f3b3524963301102fa3d52a7265b8ed42fd403
Hemen-S <hemensol1515@gmail.com>
2022-03-10 12:45:27 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,33 @@
+#include
+
+/**
+ * main - Entry point
+ *
+ * Description: to print the size of the files
+ * Return: 0
+ */
+int main(void)
+{
+	int intType;
+
+	    float floatType;
+
+	        double doubleType;
+
+		    char charType;
+
+
+
+		        // sizeof evaluates the size of a variable
+			//
+			//     printf("Size of int: %zu bytes\n", sizeof(intType));
+			//
+			//         printf("Size of float: %zu bytes\n", sizeof(floatType));
+			//
+			//             printf("Size of double: %zu bytes\n", sizeof(doubleType));
+			//
+			//                 printf("Size of char: %zu byte\n", sizeof(charType));
+			//
+			return (0);
+}			
+			                     
