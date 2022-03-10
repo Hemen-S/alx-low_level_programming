ec6493464aa72228a59f8509fa3ac09d855f14d3
Hemen-S <hemensol1515@gmail.com>
2022-03-10 11:10:55 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 
 #include <stdio.h>
 /**
- * to print a statement 
+ * main - it is the starting point
+ *
+ * description: to print a statement
+ *
+ * return: success 
  * 
 */
-int main(void) {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
