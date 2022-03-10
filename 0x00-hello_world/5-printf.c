b2b476f3e518fa0d575766df3c3770ae26cf4a52
Hemen-S <hemensol1515@gmail.com>
2022-03-10 12:31:12 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Description - prints "with proper grammar, but the outcome is a piece of art,
- * "
+ * Description - "with proper grammar, but the outcome is a piece of art,"
+ * 
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
