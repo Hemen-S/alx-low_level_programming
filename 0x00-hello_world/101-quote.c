222cbba285b720ea12a98847815c6625d64de2e8
Hemen-S <hemensol1515@gmail.com>
2022-03-10 14:18:48 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the quote
+ *
+ * Description: print Dora's saying
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return (1);
+}	
