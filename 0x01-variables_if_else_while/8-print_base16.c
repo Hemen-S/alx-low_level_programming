2b1283a7e67e5cf959fa2fa2963f46b6c79f43b5
Hemen-S <hemensol1515@gmail.com>
2022-03-11 13:31:07 -0800

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -8,4 +9,15 @@
 
 int main(void)
 {
-	int = 
+	int i = 0;
+
+	while (i < 48)
+	{
+		if (i < 10)
+			putchar(i + '0');
+		else if (i > 41)
+			putchar(i - 10 + 'A');
+		i++;
+	}
+	putchar(10);
+	return
