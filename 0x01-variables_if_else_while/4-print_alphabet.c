571e753ba1610deee24213a0d3784478a30dc76e
Hemen-S <hemensol1515@gmail.com>
2022-03-11 10:40:33 -0800

    except 1 and e

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Description: print all lowercase aphabet except q and e
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+			i++;
+		}
+	}
+	putchar(10);
+	return (0);
+}
