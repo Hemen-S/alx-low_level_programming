e809a7e150e59d39a5eab8cedad25f473cf03d5b
Hemen-S <hemensol1515@gmail.com>
2022-03-11 05:37:59 -0800

    positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * 
+ * Description: to determine if a number is either positive or negative
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 0;
+	if (n>0) {
+		printf(n, "is positive\n")
+	}
+	if (n=0) {
+		printf(n, "is zero\n")
+	}
+	if (n<0) {
+		printf(n, "is negative\n")
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+this whole program is based on the usage of if, while or else variables
