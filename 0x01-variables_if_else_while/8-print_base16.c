0bbd6b0e4227d537c982562b295d13152a0f06b7
Hemen-S <hemensol1515@gmail.com>
2022-03-11 13:18:28 -0800

    comb

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print numbers having base sixteen in lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int = 
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: all possible combination of single digits 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
