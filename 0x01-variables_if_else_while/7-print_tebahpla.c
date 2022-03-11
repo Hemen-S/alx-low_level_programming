3ab9d07fad6843ed759aa1fb4afc25a755005a9f
Hemen-S <hemensol1515@gmail.com>
2022-03-11 11:49:28 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints reverse lowercase alphabets
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 122;
+	while (i > 65)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+	return (0);
+}
