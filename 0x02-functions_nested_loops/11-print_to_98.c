746ffc3e352036d02d12f46b44fa9e42292a4849
Hemen-S <hemensol1515@gmail.com>
2022-03-15 16:35:43 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			printf("%d, \n", n);
+			printf("%d, ", n);
 			n++;
 		}
 	}
@@ -21,11 +21,10 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%d, \n", n);
+			printf("%d, ", n);
 			n--;
 		}
 	}	
-
 	printf("98\n");	
 }
 
