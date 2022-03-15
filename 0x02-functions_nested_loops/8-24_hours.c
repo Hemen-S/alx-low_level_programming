d0fdbfd0a693eb114a5a3e79fd52ad9ed8aef356
Hemen-S <hemensol1515@gmail.com>
2022-03-15 15:15:53 -0700

    24 hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of jack bauer's day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hours_tens, hours_ones, minute_tens, minute_ones, hours_max;
+
+	hours_max = 58;
+	hoours_tens = '0';
+	while (hours_tens < '3')
+	{
+		if (hours_tens == '2')
+		{
+			hours_max = '4';
+		}
+		hours_ones = '0';
+		while(hours_ones < hours_max)
+		{
+			minute_tens = '0';
+			while (minute_tens < '6')
+			{
+				minute_ones = '0';
+				while (minute_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hour_ones);
+					_putchar(':');
+					_putchar(minute_tens);
+					_putchar(minute_ones);
+					_putchar('\n');
+					minute_ones++;
+				}
+				minute_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
+	}
+}
+
+
+
