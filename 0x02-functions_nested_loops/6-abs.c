96783a87240cf12558a21746216dac1917b2de5c
Hemen-S <hemensol1515@gmail.com>
2022-03-15 14:29:47 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - determines absolute value of numebrs
+ * @f - is an integer
+ *
+ * Return: void
+ */
+
+int _abs(int)
+{
+	if (f > 0)
+	{
+		return (f);
+	}
+	else if (f < 0)
+	{
+		return (-f);
+	}
+	else
+	{
+		return (0);
+	}
+}
