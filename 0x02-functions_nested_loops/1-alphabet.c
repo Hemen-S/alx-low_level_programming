061387022c7480b9df48110479b06f21b985290b
Hemen-S <hemensol1515@gmail.com>
2022-03-15 10:44:33 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: print alphabet in lower case
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
+
