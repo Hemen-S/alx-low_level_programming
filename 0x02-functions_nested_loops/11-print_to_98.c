4f5d4279617e016d4cf5ec96a64295e9a962803f
Hemen-S <hemensol1515@gmail.com>
2022-03-15 16:13:12 -0700

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: is an integer
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	for (n=0; n < 99; n++)
+	{
+		printf("\n", n)
+	}
+	return (0);
+}
+
