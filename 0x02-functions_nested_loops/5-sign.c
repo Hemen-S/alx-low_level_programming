4e83c6b0b7179cfad833d360f5fe68c3143eb7ab
Hemen-S <hemensol1515@gmail.com>
2022-03-15 13:19:32 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n - is an ascii character
+ *
+ * Return: 1 if postive, 0 if 0 and -1 if negative
+ */
+
+int print_sign(int n)
+{
+	int i = 0;
+
+	if (i > 0)
+	{
+		return (1);
+	}
+	else if (i < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
