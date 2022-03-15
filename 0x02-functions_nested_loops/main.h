991afbb892cad48b97ddc7e3e63af31866187032
Hemen-S <hemensol1515@gmail.com>
2022-03-15 08:53:51 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,8 +2,12 @@ include <unistd.h>
 
 /**
  * _putchar - prints out given characters
+ * @c: the character to print
  * Return: Always 0
- *
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
-
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
