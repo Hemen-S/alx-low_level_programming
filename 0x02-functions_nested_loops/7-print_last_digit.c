a9a0d1cda25b5cca762efa47cf28f4c23533649c
Hemen-S <hemensol1515@gmail.com>
2022-03-15 14:49:14 -0700

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints last digit 
+ * print_last_digit - prints last digit
  * @n: is an integer
  *
  * Return: void
@@ -9,7 +9,9 @@
 
 int print_last_digit(int n)
 {
-	int poli  = n % 10; 
+	int poli = n % 10; 
+	if (n < 0)
+		poli = poli * -1;
 
 		_putchar('0' + poli);
 
