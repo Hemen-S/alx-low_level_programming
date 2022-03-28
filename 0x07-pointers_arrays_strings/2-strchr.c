493c102f99bc2a1ec7d9337f72e1320248b2c901
Hemen-S <hemensol1515@gmail.com>
2022-03-28 10:53:07 -0700

    edit 3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,22 +4,15 @@
  * _strchr - 
  * @s: pointer
  * @c: character
- * @n: size
  * Return: c[0] or NULL
  */
 
 char *_strchr(char *s, char c);
 {
-    int i;
 
-    for (i = 0; i < n; i++)
-    {
-        if (s[i] = c)
-        {
-            return (c);
-        }
-        else
-        { 
-        return ('\0')
-        }
+    for (*s == c; s++)
+        return (s);
+        
+    if (c == '\0')
+        return (0);
 }
