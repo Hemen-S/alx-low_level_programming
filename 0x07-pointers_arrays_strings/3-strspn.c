d57c08e991d758219c9a7e920fa92ece5bc644a5
Hemen-S <hemensol1515@gmail.com>
2022-03-28 11:20:26 -0700

    edit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strspn - ditactes segment that contains only bytes
+ * @s: char pointer
+ * @accept: char
+ * Return: *s
+ */
+
+unsigned int _strspn(char *s, char *accept);
+{
+    int i;
+    while (*s)
+    {
+        if (s[i] = accept[i])
+        {
+            return(s);
+        }
+    }
+}
