caf07f57c568f160095ccc53a1d55f5cf063d9e9
Hemen-S <hemensol1515@gmail.com>
2022-03-28 09:21:48 -0700

    edit 3

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * memcpy - 
+ * memcpy - to copy memory address
  * @src: char
  * @dest: char
  * @n: unsigned int
+ * Return: char
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
-    unsigned int i;
-    for(i = 0; i < n && src[i] = '\0'; i++)
-            dest[i] = src[i];   
+                unsigned int i;
+
+        for (i = 0; i < n; i++)
+                dest[i] = src[i];   
 
     return(dest);    
 }
