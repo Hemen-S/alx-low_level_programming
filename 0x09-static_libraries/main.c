aeb6e4559350b5ddb3214c1a1c3d59920456cb8b
Hemen-S <hemensol1515@gmail.com>
2022-04-02 14:16:23 -0700

    edits

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,11 +1,16 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * main - prints name
- * Return: 0
+ * main - prints name followed by a new line
+ * Return: the output (0)
+ * @puts: prints a statement
  */ 
 
 int main(void)
-{
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+{	
+
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+
 }
